1d12a320ddb295fd795646c0cdc9ecca387f4795
Elif Naz Saribas <elifnazsaribas@gmail.com>
2025-11-06 18:20:52 +0300

    added new Login page, fixed comic.png path, and removed agree section

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import LoginSignUp from './Components/Pages/LoginSignUp/LoginSignUp'
+import Login from './Components/Pages/Login/Login'
+
+
 
 function App() {
   return (
     <div> 
+      {/* for now both components are rendered one below the other I’ll connect them later*/}
         <Navbar/>
         <LoginSignUp/>
+        <Navbar/>
+        <Login/>
         
     </div>
   );
 }
 
 export default App;
+
