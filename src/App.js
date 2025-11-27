740147083144d756743c46b7b472e3d07f212872
eceguneyyy <ece.guneyy05@gmail.com>
2025-11-27 21:52:16 +0300

    search page created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from './Components/Pages/Login/Login';
 import LoginSignUp from './Components/Pages/LoginSignUp/LoginSignUp';
 import Home from "./Components/Pages/Home/Home";
 import Cart from "./Components/Pages/Cart/Cart";
+import SearchPage from "./Components/Pages/Search/SearchPage";
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<LoginSignUp />} />
         <Route path="/cart" element={<Cart />} /> 
+       <Route path="/search" element={<SearchPage />} />
       </Routes>
     </Router>
   );
