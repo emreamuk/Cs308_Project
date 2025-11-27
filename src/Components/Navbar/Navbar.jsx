0bcc192bb775e720fb82b5f3305c671f1d50bd36
Elif Naz Saribas <elifnazsaribas@gmail.com>
2025-11-27 20:21:58 +0300

    Added a search icon

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import "./Navbar.css"
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import search_icon from '../Assets/search_icon.png'
 import { Link } from 'react-router-dom'; 
+
 const Navbar = () => {
   return (
     <div className ='navbar'>
@@ -10,18 +12,33 @@ const Navbar = () => {
             <img src={logo} alt=""/>
             <p>AO Comics</p>
         </div>
+
         <ul className="nav-menu">
             <li>Shop</li>
             <li>Comics</li>
             <li>Books</li>
             <li>Games</li>
         </ul>
-        <div className='nav-login-cart'>
-            <Link to="/login">
-            <button>Login</button>
-            </Link>
-            <img src= {cart_icon} alt="" />
-        </div>
+        
+    <div className='nav-login-cart'>
+                <Link to="/login">
+                    <button>Login</button>
+                </Link>
+
+                {/* Search Icon */}
+                <img 
+                    className="nav-search-icon" 
+                    src={search_icon} 
+                    alt="Search" 
+                />
+
+                {/* Cart Icon */}
+                <img 
+                    className="nav-cart-icon" 
+                    src={cart_icon} 
+                    alt="Cart" 
+                />
+            </div>
     </div>
   )
 }
