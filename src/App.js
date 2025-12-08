f9d19ea2f8f52bdb17233dc5fd459c2b2fb4970d
eceguneyyy <ece.guneyy05@gmail.com>
2025-12-08 23:26:22 +0300

    Checkout page added and route updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Login from './Components/Pages/Login/Login';
 import LoginSignUp from './Components/Pages/LoginSignUp/LoginSignUp';
+import Home from "./Components/Pages/Home/Home";
+import Cart from "./Components/Pages/Cart/Cart";
+import SearchPage from "./Components/Pages/Search/SearchPage";
+
 
 function App() {
   return (
-    <div className="App"> 
-        <LoginSignUp/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<LoginSignUp />} />
+        <Route path="/cart" element={<Cart />} /> 
+       <Route path="/search" element={<SearchPage />} />
+      </Routes>
+    </Router>
   );
 }
 
