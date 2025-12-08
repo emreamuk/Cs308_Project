a7da204a62d89f67f99fc6dfaf94786402b40b57
Hormoaning <mertkaras2004@gmail.com>
2025-12-08 15:50:42 +0300

    Adding role field and other features to User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,6 +5,9 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  taxID: String,
+  homeAddress: String,
+  role: { type: String, default: 'customer' }, // customer, sales_manager, product_manager, support_agent
   createdAt: { type: Date, default: Date.now }
 });
 
